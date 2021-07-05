57f8e0579e9471fa8cad84834fce0e8e3271952d
TobiasUng <44364683+TobiasUng@users.noreply.github.com>
2021-07-05 14:14:35 +0200

    init

---
+++ tutorials/pick_and_place/PickAndPlaceProject/Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    private Ray ray;
+    public LayerMask buttonLayerMask;
+    //public Material hover;
+    //public Material prevHover;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 10f, buttonLayerMask))
+        {
+
+            
+            
+            
+            /*prevHover = button.GetComponent<Renderer>().material;
+            button.GetComponent<Renderer>().material = hover;*/
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GameObject button = hit.transform.gameObject;
+                button.GetComponent<TrajectoryPlanner>().PublishJoints();
+            }
+        }
+
+    }
+
+    private void OnMouseDown()
+    {
+       
+        
+    }
+}
