d959ed79f3de1702f7511055b4a91546fa82f9ad
TobiasUng <44364683+TobiasUng@users.noreply.github.com>
2021-09-03 10:49:36 +0200

    Collision detection with screw and player

---
+++ tutorials/pick_and_place/PickAndPlaceProject/Assets/LockRotation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockRotation : MonoBehaviour
+{
+    Quaternion iniRot;
+ 
+    void Start()
+    {
+        iniRot = transform.rotation;
+    }
+
+    void LateUpdate()
+    {
+        transform.rotation = iniRot;
+    }
+}
--- tutorials/pick_and_place/PickAndPlaceProject/Assets/Scripts/RandomColor.cs
+++ tutorials/pick_and_place/PickAndPlaceProject/Assets/Scripts/RandomColor.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomColor : MonoBehaviour
+public class ScrewCollision : MonoBehaviour
 {
 
-    public Material[] colors;
+    
     
 
     // Start is called before the first frame update
