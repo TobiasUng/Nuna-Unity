e10d746589ee79822c9f1965edacf9d4b4a58855
TobiasUng <44364683+TobiasUng@users.noreply.github.com>
2021-10-11 16:24:40 +0200

    blabla

--- tutorials/pick_and_place/PickAndPlaceProject/Assets/Scripts/PlayerStats.cs
+++ tutorials/pick_and_place/PickAndPlaceProject/Assets/Scripts/PlayerStats.cs
@@ -10,7 +10,7 @@ public class PlayerStats : MonoBehaviour
     public static string fileName;
     public static float startTime;
     public static float endTime;
-    public static string participant = "P2";
+    public static string participant = "P4";
 
 
     public void Start()
