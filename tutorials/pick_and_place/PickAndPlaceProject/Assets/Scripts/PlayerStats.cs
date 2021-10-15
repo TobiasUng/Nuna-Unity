737a2a3b2ca37e6ed1369a5edf027a94820c4dc4
TobiasUng <44364683+TobiasUng@users.noreply.github.com>
2021-10-15 07:08:38 -0700

    study day 3

--- tutorials/pick_and_place/PickAndPlaceProject/Assets/Scripts/PlayerStats.cs
+++ tutorials/pick_and_place/PickAndPlaceProject/Assets/Scripts/PlayerStats.cs
@@ -10,7 +10,7 @@ public class PlayerStats : MonoBehaviour
     public static string fileName;
     public static float startTime;
     public static float endTime;
-    public static string participant = "P11";
+    public static string participant = "P13";
 
 
     public void Start()
--- tutorials/pick_and_place/PickAndPlaceProject/Assets/Scripts/Rumbler.cs
+++ tutorials/pick_and_place/PickAndPlaceProject/Assets/Scripts/Rumbler.cs
@@ -11,20 +11,20 @@ public class Rumbler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        List<InputDevice> devices = new List<InputDevice>();
+        /*List<InputDevice> devices = new List<InputDevice>();
         InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
         InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
         rightController = devices[0];
 
         InputDeviceCharacteristics leftControllerCharacteristics = InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
         InputDevices.GetDevicesWithCharacteristics(leftControllerCharacteristics, devices);
-        leftController = devices[0];
+        leftController = devices[0];*/
     }
 
     public void shortRumble()
     {
-        Debug.Log("rumble");
+        /*Debug.Log("rumble");
         leftController.SendHapticImpulse(0u, 0.5f, 0.1f);
-        rightController.SendHapticImpulse(0u, 0.5f, 0.1f);
+        rightController.SendHapticImpulse(0u, 0.5f, 0.1f);*/
     }
 }
